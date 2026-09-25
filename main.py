54bc927a73c8f7407751136a93dd543487e0a7ae
christinabrooman <cbrooma@wgu.edu>
2026-09-25 11:40:05 -0400

    one more flake* inline fix

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ async def post_inference(data: Data):
     # Here it uses the functionality of FastAPI/Pydantic/etc to deal with this.
     data = {k.replace("_", "-"): [v] for k, v in data_dict.items()}
     data = pd.DataFrame.from_dict(data)
-"""
+    """
     cat_features = [
         "workclass",
         "education",
